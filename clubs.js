94501ef790248648ef8b0f18c59f120a5f09e72c
Richard Lin <richardlin4375@gmail.com>
2023-02-16 18:17:49 -0500

    Final changes, all mobile, contact, menu

--- clubs.js
+++ clubs.js
@@ -1,28 +1,36 @@
+var offset;
+if (width >= 1024) {
+    offset = 125;
+}
+else {
+    offset=300;
+}
 document.getElementById("academicscroll").onmousedown = function(){
-    scrollTo({top: document.getElementById("clubsec1").offsetTop - 100, behavior: "smooth"});
+    scrollTo({top: document.getElementById("clubsec1").offsetTop - offset, behavior: "smooth"});
 };
 
 document.getElementById("culturalscroll").onmousedown = function(){
-    scrollTo({top: document.getElementById("clubsec2").offsetTop - 100, behavior: "smooth"});
+    scrollTo({top: document.getElementById("clubsec2").offsetTop - offset, behavior: "smooth"});
 };
 
 document.getElementById("sjscroll").onmousedown = function(){
-    scrollTo({top: document.getElementById("clubsec3").offsetTop - 100, behavior: "smooth"});
+    scrollTo({top: document.getElementById("clubsec3").offsetTop - offset, behavior: "smooth"});
 };
 
 document.getElementById("volunteerscroll").onmousedown = function(){
-    scrollTo({top: document.getElementById("clubsec4").offsetTop - 100, behavior: "smooth"});
+    scrollTo({top: document.getElementById("clubsec4").offsetTop - offset, behavior: "smooth"});
 };
 
 document.getElementById("leisurescroll").onmousedown = function(){
-    scrollTo({top: document.getElementById("clubsec5").offsetTop - 100, behavior: "smooth"});
+    scrollTo({top: document.getElementById("clubsec5").offsetTop - offset, behavior: "smooth"});
 };
 
 document.getElementById("newscroll").onmousedown = function(){
-    scrollTo({top: document.getElementById("clubsec6").offsetTop - 100, behavior: "smooth"});
+    scrollTo({top: document.getElementById("clubsec6").offsetTop - offset, behavior: "smooth"});
 };
 
+
 document.getElementById("backtop").onmousedown = function(){
-    scrollTo({top: 0, behavior: "smooth"});
+    scrollTo({top: document.getElementById("scrolling").offsetTop - offset, behavior: "smooth"});
 };
 
--- universal.js
+++ universal.js
@@ -1,22 +1,51 @@
 let width = window.screen.width;
 if (width > 1024) {
     setInterval(function () {
-    if (window.scrollY <= 50) {
-        document.getElementById("username").style="font-size: 35px; ";
-        document.getElementById("menu").style = "box-shadow: none; padding-top: 60px; padding-bottom: 80px;";
-        document.getElementById("tabs").style="margin-top: 0px;";
-    }
-    else {
-        document.getElementById("menu").style = "box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4); padding-top: 0px; padding-bottom: 0px; height: 90px;";
-        document.getElementById("tabs").style="margin-top: 5px;";
-        document.getElementById("username").style="font-size: 0;";
-        
-    }
-}, 10)
+        if (window.scrollY <= 50) {
+            document.getElementById("username").style="font-size: 35px; ";
+            document.getElementById("menu").style = "box-shadow: none; padding-top: 60px; padding-bottom: 80px;";
+            document.getElementById("tabs").style="margin-top: 0px;";
+            document.getElementById("menuimage").style = "width: 50px;";
+        }
+        else {
+            document.getElementById("menu").style = "box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.4); padding-top: 0px; padding-bottom: 0px; height: 90px;";
+            document.getElementById("tabs").style="margin-top: 15px;";
+            document.getElementById("username").style="font-size: 0;";
+            document.getElementById("menuimage").style = "width: 0px;";
+            
+        }
+    }, 10)
 }
-else {
 
+/*menu*/
+var openm = false;
+function openmenu() {
+    if (openm == false) {
+        document.getElementById("menu").style.height = "1900px";
+        openm = true;
+        document.getElementById("menubutton").style.transform = "rotate(-90deg)";
+    }
+    else if (openm == true) {
+        document.getElementById("menu").style.height = "140px";
+        openm = false;
+        document.getElementById("menubutton").style.transform = "rotate(90deg)";
+    }
 }
+
+setInterval(function() {
+    let width = window.screen.width;
+    if (width < 1024 && openm == true) {
+        document.getElementById("menu").style.height = "1900px";
+        document.getElementById("menu").style.backgroundColor = "var(--main)";
+    }
+    else if (width > 1024) {
+        document.getElementById("menu").style.height = "85px";
+    }
+
+}, 10);
+
+
+
 
 function wait(ms){
     var start = new Date().getTime();
