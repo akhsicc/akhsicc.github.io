30919f688e699aeb6de557ec058d045a2e503189
boomermath <63861512+boomermath@users.noreply.github.com>
2025-03-06 11:10:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.getElementById("link").onmousedown = function(){
     window.open("https://akicc.weebly.com/uploads/1/2/7/1/127136347/new_icc_constitution_draft_-_summer_2020.pdf", "_blank");
 };
 document.getElementById("link1").onmousedown = function(){
-    window.open("https://docs.google.com/document/d/1448bY-W-eDId6RfV6zeMQvsLQB0UgdD4BO0nibjn8NQ/edit", "_blank");
+   // window.open("https://docs.google.com/document/d/1448bY-W-eDId6RfV6zeMQvsLQB0UgdD4BO0nibjn8NQ/edit", "_blank");
 };
 document.getElementById("link2").onmousedown = function(){
     window.open("https://www.signupgenius.com/go/10C0E4EA4A92FA6F8C34-55260845-iccspring", "_blank");
